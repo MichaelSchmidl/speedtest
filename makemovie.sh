74acb251c0263abbfeee013a100c47cd904fd7e8
Michael Schmidl <Mich@el-Schmidl.de>
2018-04-22 16:01:09 +0200

    added a script to generate a movie out of the data

---
+++ makemovie.sh
@@ -0,0 +1,45 @@
+TMPDIR=tmp
+
+rm -rf $TMPDIR
+mkdir $TMPDIR
+cd $TMPDIR
+
+CSV_FILE="../speedtest.csv"
+MEASUREMENTS_PER_HOUR=4
+
+MEASUREMENTS_PER_DAY=$((MEASUREMENTS_PER_HOUR*24))
+MEASUREMENTS_PER_WEEK=$((MEASUREMENTS_PER_DAY*7))
+
+TOTAL_MEASUREMENTS=`awk 'END {print NR}' $CSV_FILE`
+
+FRAME_INCREMENT=$MEASUREMENTS_PER_HOUR
+FRAME_START=$MEASUREMENTS_PER_WEEK
+
+rm -rf *.png
+
+printf "            Input file: $CSV_FILE\n"
+printf "Number of measurements: $TOTAL_MEASUREMENTS\n"
+printf "       Number of hours: $((TOTAL_MEASUREMENTS/$MEASUREMENTS_PER_HOUR))\n"
+printf "        Number of days: $((TOTAL_MEASUREMENTS/$MEASUREMENTS_PER_DAY))\n"
+printf "       Number of weeks: $((TOTAL_MEASUREMENTS/$MEASUREMENTS_PER_WEEK))\n"
+printf "            FrameStart: $FRAME_START\n"
+printf "              FrameInc: $FRAME_INCREMENT\n"
+printf "       expected frames: $(((TOTAL_MEASUREMENTS-$FRAME_START)/$FRAME_INCREMENT))\n"
+
+
+FRAME_NR=0
+CNT=$FRAME_START
+while [ $CNT -le $TOTAL_MEASUREMENTS ]; do
+   head -n $CNT $CSV_FILE >speedtest.csv
+   gnuplot ../speedtest.gnu
+   frame="$(printf "frame%05d.png" $FRAME_NR)"
+   printf "$frame \r"
+   cp speedtest.png $frame
+   CNT=$((CNT+$FRAME_INCREMENT))
+   FRAME_NR=$((FRAME_NR+1))
+done
+
+printf "generating movie now\n"
+convert -quality 100 -delay 5 *.png speedtest.mpg
+
+cp speedtest.mpg ..

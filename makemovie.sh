c14ee3eda8c9700992630479b8bfcdac4c8eaae3
Michael Schmidl <Mich@el-Schmidl.de>
2018-04-24 20:17:02 +0200

    nice status output while rendering movie

--- makemovie.sh
+++ makemovie.sh
@@ -26,20 +26,19 @@ printf "            FrameStart: $FRAME_START\n"
 printf "              FrameInc: $FRAME_INCREMENT\n"
 printf "       expected frames: $(((TOTAL_MEASUREMENTS-$FRAME_START)/$FRAME_INCREMENT))\n"
 
-
 FRAME_NR=0
 CNT=$FRAME_START
 while [ $CNT -le $TOTAL_MEASUREMENTS ]; do
    head -n $CNT $CSV_FILE >speedtest.csv
    gnuplot ../speedtest.gnu
    frame="$(printf "frame%05d.png" $FRAME_NR)"
-   printf "$frame \r"
+   printf "       rendering frame: $frame \r"
    cp speedtest.png $frame
    CNT=$((CNT+$FRAME_INCREMENT))
    FRAME_NR=$((FRAME_NR+1))
 done
 
-printf "generating movie now\n"
+printf "\ngenerating movie now\n"
 convert -quality 100 -delay 5 *.png speedtest.mpg
 
 cp speedtest.mpg ..
